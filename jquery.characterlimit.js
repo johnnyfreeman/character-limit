d4cb170b5b51e71300bb8e65acb1e5a8c65e77da
Johnny Freeman <prsjohnny@gmail.com>
2012-01-18 13:10:40 -0500

    refactored entire plugin so that the prototype isnt overwritten but extended

--- jquery.characterlimit.js
+++ jquery.characterlimit.js
@@ -1,63 +1,49 @@
 ;(function($){
+
     var characterLimit = function(field, options)
     {
         this.field = $(field);
         $.extend(this.options, options);
 
         this.field.bind({
-            'keypress.characterLimit': this.keypressHandler.bind(this),
-            'keyup.characterLimit': this.keyupHandler.bind(this)
+            'keypress.characterLimit': this.updateResponse.bind(this),
+            'keyup.characterLimit': this.updateResponse.bind(this)
         });
 
-        this.response = $('<div />').attr('id', 'msg_response').text(this.options.limit + ' Characters Remaining');
+        this.response = $('<div />').addClass('characterLimit_response').text(this.options.limit + ' Characters Remaining');
         this.field.after(this.response);
     };
 
-    characterLimit.prototype = {
+    characterLimit.prototype.limit = 160; // default limit
+    characterLimit.prototype.field = null;
 
-	    options: {
-	        limit: 160
-	    },
+    characterLimit.prototype.limitReached = function()
+    {
+        return (this.field.val().length + 1) > this.options.limit;
+    };
 
-        field: null,
-        
-        keypressHandler: function(e)
+    characterLimit.prototype.updateResponse = function()
+    {
+        var remaining = this.options.limit - this.field.val().length;
+
+        if (remaining > 10)
         {
-            return this.updateResponse();
-        },
-        
-        keyupHandler: function(e)
-        {
-            return this.updateResponse();
-        },
-
-        limitReached: function()
-        {
-        	return (this.field.val().length + 1) > this.options.limit;
-        },
-
-        updateResponse: function()
-        {
-        	var remaining = this.options.limit - this.field.val().length;
-
-        	if (remaining > 10)
-        	{
-        		this.response.removeClass('error');
-        	}
-	        else
-	        {
-	        	this.response.addClass('error');
-		    };
-
-        	return this.response.text(remaining + ' Characters Remaining')
+            this.response.removeClass('error');
         }
-    }
+        else
+        {
+            this.response.addClass('error');
+        };
 
-    characterLimit.options = characterLimit.prototype.options;
+        return this.response.text(remaining + ' Characters Remaining')
+    };
 
-    $.fn.characterLimit = function(options) {
-        return this.each(function() {
+    $.fn.characterLimit = function(options)
+    {
+        return this.each(function()
+        {
             return new characterLimit(this, options);
         });
     };
+
 })(jQuery);
