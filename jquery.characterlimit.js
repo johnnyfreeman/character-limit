415c96aba20faa122d29966c1980817a9bb5fbdc
Johnny Freeman <prsjohnny@gmail.com>
2012-01-18 17:01:20 -0500

    added options object to make room for new planned options

--- jquery.characterlimit.js
+++ jquery.characterlimit.js
@@ -1,34 +1,33 @@
 ;(function($){
 
-    var characterLimit = function(field, limit)
+    var characterLimit = function(field, options)
     {
         this.field = $(field);
-
-        if (typeof limit !== 'undefined')
-        {
-            this.limit = limit;
-        };
+        $.extend(this.options, options);
 
         this.field.bind({
             'keypress.characterLimit': this.updateResponse.bind(this),
             'keyup.characterLimit': this.updateResponse.bind(this)
         });
 
-        this.response = $('<div />').addClass('characterLimit_response').text(this.limit + ' Characters Remaining');
+        this.response = $('<div />').addClass('characterLimit_response').text(this.options.limit + ' Characters Remaining');
         this.field.after(this.response);
     };
 
-    characterLimit.prototype.limit = 160; // default limit
+    characterLimit.prototype.options = {
+        limit: 160 // default limit
+    };
+   
     characterLimit.prototype.field = null;
 
     characterLimit.prototype.limitReached = function()
     {
-        return (this.field.val().length + 1) > this.limit;
+        return (this.field.val().length + 1) > this.options.limit;
     };
 
     characterLimit.prototype.updateResponse = function()
     {
-        var remaining = this.limit - this.field.val().length;
+        var remaining = this.options.limit - this.field.val().length;
 
         if (remaining > 10)
         {
@@ -42,16 +41,12 @@
         return this.response.text(remaining + ' Characters Remaining')
     };
 
-    $.fn.characterLimit = function(limit)
+    $.fn.characterLimit = function(options)
     {
         return this.each(function()
         {
-            return new characterLimit(this, limit);
+            return new characterLimit(this, options);
         });
     };
-     
-     $(document).ready(function(){
-         $('#tweet').characterLimit(10);
-     });
 
 })(jQuery);
